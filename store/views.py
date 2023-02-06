43d91e1d60f7209ca4cfa7ddb46b8b0d640bb196
henrysevern <henrysevern2@gmail.com>
2023-02-06 14:42:24 +0000

    Create view to render item_list.html

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def item_list(request):
+    context = {
+        'items':Item.objects.all()
+    }
+    return render(request, "store/item_list.html", context)
 
-
-def home(request):
-    return render(request, 'store/base.html')
