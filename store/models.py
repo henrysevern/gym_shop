b7c323be897cebdb453c985868e471f05be3259c
henrysevern <henrysevern2@gmail.com>
2023-02-12 21:19:41 +0000

    Add image field to product model

--- store/models.py
+++ store/models.py
@@ -9,7 +9,8 @@ class Customer(models.Model):
     """
     Customer model
     """
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True,
+                                on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
 
@@ -27,27 +28,39 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+    # Allows the page to load without error if the item contains no image
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
     """
     Order model
     """
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                 blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class OrderItem(models.Model):
     """
     Order item model
     """
-    product = models.ForeignKey(Item, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(Item, on_delete=models.SET_NULL, blank=True,
+                                null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True,
+                              null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -56,8 +69,10 @@ class ShippingAddress(models.Model):
     """
     Shipping address model
     """
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                 blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True,
+                              null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     county = models.CharField(max_length=200, null=True)
