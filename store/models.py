bd3f4c0fe747b0f293191a4941a0dab26ece7e52
henrysevern <henrysevern2@gmail.com>
2023-02-12 18:40:53 +0000

    Change model set up

--- store/models.py
+++ store/models.py
@@ -1,9 +1,27 @@
-from django.conf import settings 
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Customer(models.Model):
+    """
+    Customer model
+    """
+    user = models.OneToOneField(User, null=True, blank=True,
+                                on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
+    """
+    Items model
+    """
     title = models.CharField(max_length=100)
     price = models.FloatField()
 
@@ -11,19 +29,45 @@ class Item(models.Model):
         return self.title
 
 
-class OrderItem(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    items = models.ManyToManyField(OrderItem)
-    start_date = models.DateTimeField(auto_now_add=True)
-    ordered_date = models.DateTimeField()
-    ordered = models.BooleanField(default=False)
+    """
+    Order model
+    """
+    customer = model.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.user.username
+        return self.id
+
+
+class OrderItem(model.Model):
+    """
+    Order item model
+    """
+    product = model.ForeignKey(Item, on_delete=models.SET_NULL,
+                               blank=True, null=True)
+    order = model.ForeignKey(Order, on_delete=models.SET_NULL,
+                             blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(model.Model):
+    """
+    Shipping address model
+    """
+    customer = model.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                blank=True, null=True)
+    order = model.ForeignKey(Order, on_delete=models.SET_NULL,
+                             blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    county = models.CharField(max_length=200, null=True)
+    postcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
