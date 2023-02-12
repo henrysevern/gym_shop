438f1432e22f48c63742ab1a0b41613e101a1fa0
henrysevern <henrysevern2@gmail.com>
2023-02-12 20:19:15 +0000

    Add views for cart and checkout pages

--- store/views.py
+++ store/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from .models import Item
+from .models import *
+# from .models import Item
+
 
 def item_list(request):
+    items = Item.objects.all()
     context = {
-        'items':Item.objects.all()
+        'items': items
     }
     return render(request, "store/item_list.html", context)
 
+
+def cart(request):
+    context = {}
+    return render(request, "store/cart.html", context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, "store/checkout.html", context)
