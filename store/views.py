2aaab1af1d29af9936beef40d8289cf33173f63a
henrysevern <henrysevern2@gmail.com>
2023-02-13 22:23:11 +0000

    Create update cart view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 from .models import *
 # from .models import Item
 
@@ -46,4 +47,28 @@ def checkout(request):
 
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action', action)
+    print('productId', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer,
+                                                 complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order,
+                                                         product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
