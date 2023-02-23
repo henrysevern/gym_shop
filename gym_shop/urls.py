d100992049d7ce659d45857b5ac30e47f4f1a517
henrysevern <henrysevern2@gmail.com>
2023-02-23 16:10:19 +0000

    Include profiles and payments urls in main project urls

--- gym_shop/urls.py
+++ gym_shop/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import store
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('store.urls')),
+    path('payments/', include('payments.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
