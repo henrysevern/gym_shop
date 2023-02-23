eeb65c2facffc4a933c2050472c1b9c3b65127f7
henrysevern <henrysevern2@gmail.com>
2023-02-23 16:06:16 +0000

    Create profile model to store personal information

--- profiles/models.py
+++ profiles/models.py
@@ -1,25 +1,26 @@
-# from django.db import models
-# from django.contib.auth.models import User
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
-# class UserProfile(models.Model):
+class UserProfile(models.Model):
 
-#     """
-#     A user profile model for maintaining default
-#     delivery information and order history
-#     """
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True,
-#                               null=True)
-#     default_address = models.CharField(max_length=200, null=True, blank=True)
-#     default_city = models.CharField(max_length=200, null=True, blank=True)
-#     default_county = models.CharField(max_length=200, null=True, blank=True)
-#     default_postcode = models.CharField(max_length=200, null=True, blank=True)
-#     date_added = models.DateTimeField(auto_now_add=True)
+    """
+     A user profile model for maintaining default
+     delivery information and order history
+     """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, null=True, blank=True)
+    last_name = models.CharField(max_length=15, null=True, blank=True)
+    phone = PhoneNumberField(null=True, blank=True)
+    default_address = models.CharField(max_length=200, null=True, blank=True)
+    default_city = models.CharField(max_length=200, null=True, blank=True)
+    default_county = models.CharField(max_length=200, null=True, blank=True)
+    default_postcode = models.CharField(max_length=200, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
 
 
 # @receiver(post_save, sender=User)
