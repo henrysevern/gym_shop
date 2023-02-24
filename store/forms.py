0191b05493050122d2650c22e3ac955d0516387f
henrysevern <henrysevern2@gmail.com>
2023-02-24 12:17:13 +0000

    Create form for adding a product

--- store/forms.py
+++ store/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Product
 from django import forms
 
 
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
