69dc5a53c19ff11c34857d0ef14cbecca7e7a25f
henrysevern <henrysevern2@gmail.com>
2023-02-24 16:18:57 +0000

    Fix comment section not displaying properly

--- store/admin.py
+++ store/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+from . import models
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'approved', 'created_on',)
+
 
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(ShippingAddress)
-admin.site.register(Comment)
