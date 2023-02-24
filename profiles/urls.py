d1cc8f09eadd3ff6e52eba4ec4a8148657d5c92e
henrysevern <henrysevern2@gmail.com>
2023-02-24 16:13:16 +0000

    Create url for edit profile template

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('update/', views.profileUpdate.as_view(),
-         name="profile-update")
+    path('update/<int:profile_id>', views.edit_profile,
+         name="edit_profile")
 ]
