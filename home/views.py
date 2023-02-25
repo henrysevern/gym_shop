6285e5ae3c38557cddc3458f510b60bd153a413e
henrysevern <henrysevern2@gmail.com>
2023-02-25 01:21:21 +0000

    Create view and url to render home landing page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+
+def index(request):
+    """
+    A view to return the index page.
+    """
+    template = 'home/index.html'
+
+    return render(request, template)
