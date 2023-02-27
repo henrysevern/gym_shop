1bc244243ab69a7ec60a6c70af7be83336e47b3e
henrysevern <henrysevern2@gmail.com>
2023-02-27 11:50:08 +0000

    Register catergory model and change list displays

--- store/admin.py
+++ store/admin.py
@@ -5,13 +5,33 @@ from .models import *
 from . import models
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'approved', 'created_on',)
 
 
 admin.site.register(Customer)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(ShippingAddress)
