63f3f31b627fabaf6892a59e790a3f4dcf309be0
henrysevern <henrysevern2@gmail.com>
2023-02-27 11:54:43 +0000

    Create catergory model

--- store/models.py
+++ store/models.py
@@ -18,12 +18,31 @@ class Customer(models.Model):
         return self.name
 
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     """
     Products model
     """
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
