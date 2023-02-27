c55efcfef4e6b8e599254ecb2372f6959c377e11
henrysevern <henrysevern2@gmail.com>
2023-02-27 16:55:12 +0000

    Add recent order tab to profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from store.models import Order
 
 from . import models
 from .models import UserProfile
@@ -14,11 +18,21 @@ def profile(request):
     """
     Display the user's profile page.
     """
+    four_weeks_ago = timezone.now() - timedelta(days=7*4)
+
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    # get orders in the last four weeks
+    recent_orders = Order.objects.filter(
+        date_ordered__gte=four_weeks_ago,
+        customer=request.user.customer,
+        complete=True
+    )
 
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
+        'recent_orders': recent_orders
     }
 
     return render(request, template, context)
