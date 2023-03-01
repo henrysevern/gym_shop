9db882e8374d5932039bf08eb4cf680a1e2ed74d
henrysevern <henrysevern2@gmail.com>
2023-03-01 00:43:55 +0000

    Change settings for debug

--- gym_shop/settings.py
+++ gym_shop/settings.py
@@ -28,10 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('HEROKU') == "true":
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = False
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gym-shop.herokuapp.com', 'localhost']
 
