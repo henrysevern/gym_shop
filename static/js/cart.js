68d2d08b3d1069985a8d4ce19e81d518a6f75991
henrysevern <henrysevern2@gmail.com>
2023-03-01 14:23:33 +0000

    Validate js files

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,25 +1,25 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
 for (i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId', productId, 'Action', action)
-        console.log('USER:', user)
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId', productId, 'Action', action);
+        console.log('USER:', user);
 
         if (user == 'AnonymousUser'){
-            console.log('User is not authenticated')
+            console.log('User is not authenticated');
             alert('Please sign up/Log in to add to cart!');
         }else{
-            updateUserOrder(productId, action)
+            updateUserOrder(productId, action);
         }
-    })
+    });
 }
 
 function updateUserOrder(productId, action){
-    console.log('User is authenticated, sending data...')
+    console.log('User is authenticated, sending data...');
 
-    var url = '/update_item/'
+    var url = '/update_item/';
 
     fetch(url,{
         method: 'POST',
@@ -31,11 +31,11 @@ function updateUserOrder(productId, action){
     })
 
     .then((response) => {
-        return response.json()
+        return response.json();
     })
 
     .then((data) => {
-        console.log('data', data)
-        location.reload()
-    })
+        console.log('data', data);
+        location.reload();
+    });
 }
--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,39 +1,35 @@
-// moved these to checkout.html
-// var shipping = '{{order.shipping}}'
-// var total = '{{order.get_cart_total}}'
-
 if (shipping == 'False') {
-    document.getElementById('shipping-info').innerHTML = ''
+    document.getElementById('shipping-info').innerHTML = '';
 }
 
 if (user != 'AnonymousUser') {
-    document.getElementById('user-info').innerHTML = ''
+    document.getElementById('user-info').innerHTML = '';
 }
 
 // Hides entire form if user is logged in and shipping is false.
 if (shipping == 'False' && user != 'AnonymousUser') {
-    document.getElementById('form-wrapper').classList.add("hidden")
-    document.getElementById('payment-info').classList.remove("hidden")
+    document.getElementById('form-wrapper').classList.add("hidden");
+    document.getElementById('payment-info').classList.remove("hidden");
 }
 // Gets the payment button displayed once the form has been filled, removes submit form button 
-var form = document.getElementById('form')
+var form = document.getElementById('form');
 
 // var csrftoken = form.getElementsByTagName("input")[0].value
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    console.log('Form submitted...')
-    submitFormData()
-})
+    e.preventDefault();
+    console.log('Form submitted...');
+    submitFormData();
+});
 
 function submitFormData() {
-    console.log('Payment button clicked')
+    console.log('Payment button clicked');
 
     var userFormData = {
         'name': null,
         'email': null,
         'total': total,
-    }
+    };
 
     var shippingInfo = {
         'address': null,
@@ -41,21 +37,21 @@ function submitFormData() {
         'country': null,
         'postcode': null,
 
-    }
+    };
 
     if (shipping != 'False') {
-        shippingInfo.address = form.address.value
-        shippingInfo.city = form.city.value
-        shippingInfo.country = form.country.value
-        shippingInfo.postcode = form.postcode.value
+        shippingInfo.address = form.address.value;
+        shippingInfo.city = form.city.value;
+        shippingInfo.country = form.country.value;
+        shippingInfo.postcode = form.postcode.value;
     }
 
     if (user == 'AnonymousUser') {
-        userFormData.name = form.name.value
-        userFormData.email = form.email.value
+        userFormData.name = form.name.value;
+        userFormData.email = form.email.value;
     }
 
-    var url = '/process_order/'
+    var url = '/process_order/';
     fetch(url, {
         method: 'POST',
         headers: {
@@ -72,6 +68,6 @@ function submitFormData() {
         .then((data) => {
             console.log('Success', data);
             alert('Shipping adress added!');
-            window.location.href = "/payments"
-        })
+            window.location.href = "/payments";
+        });
 }
