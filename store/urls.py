55a92c818184554318ce5a6ee67e5ad9cac16e26
henrysevern <henrysevern2@gmail.com>
2023-05-20 17:25:35 +0000

    Update urls for new apps

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,12 @@
-
-
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.item_list, name="item_list"),
-    path('<int:product_id>/', views.item_view, name="item_view"),
+    path('<product_id>', views.item_view, name="item_view"),
     path('product/add_comment/<int:product_id>/', views.add_comment,
          name="add_comment"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
     path("add/", views.add_product, name="add_product"),
     path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
     path("delete/<int:product_id>/", views.delete_product,
