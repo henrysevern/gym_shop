be683032c5deeb1becbd3568b245dc2065fc4bc7
henrysevern <henrysevern2@gmail.com>
2023-05-20 17:20:07 +0000

    Update store models

--- store/models.py
+++ store/models.py
@@ -1,22 +1,6 @@
-from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class Customer(models.Model):
-    """
-    Customer model
-    """
-    user = models.OneToOneField(User, null=True, blank=True,
-                                on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
-    stripe_customer_id = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Category(models.Model):
@@ -41,6 +25,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     title = models.CharField(max_length=100)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     price = models.FloatField()
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
@@ -49,94 +34,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-
-    # Allows the page to load without error if the item contains no image
-    @property
-    def imageURL(self):
-        try:
-            url = self.image.url
-        except image.url.DoesNotExist:
-            url = ''
-        return url
-
-
-class Order(models.Model):
-    """
-    Order model
-    """
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,
-                                 blank=True, null=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False, null=True, blank=False)
-    shipping = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return str(self.id)
-
-    # Gets the cart total for the cart and checkout page
-    @property
-    def get_cart_total(self):
-        orderitems = self.orderitem_set.all()
-        total = sum([item.get_total for item in orderitems])
-        return total
-
-    # Gets the item total for the cart and checkout page
-    @property
-    def get_cart_items(self):
-        orderitems = self.orderitem_set.all()
-        total = sum([item.quantity for item in orderitems])
-        return total
-
-    @property
-    def cart_total_integer(self):
-        # assume denomination is pennies
-        return int(self.get_cart_total * 100)
-
-
-class OrderItem(models.Model):
-    """
-    Order item model
-    """
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True,
-                                null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True,
-                              null=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    # Gets the total for to quantity of products in the cart
-    @property
-    def get_total(self):
-        total = self.product.price * self.quantity
-        return total
-
-
-class ShippingAddress(models.Model):
-    """
-    Shipping address model
-    """
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,
-                                 blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True,
-                              null=True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    county = models.CharField(max_length=200, null=True)
-    postcode = models.CharField(max_length=200, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.address
-
-
-class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,
-                                related_name="comments")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.username
