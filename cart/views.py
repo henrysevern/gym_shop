59fd5770ef8fb20a5467040fedd00abe713126bc
henrysevern <henrysevern2@gmail.com>
2023-05-20 16:21:06 +0000

    Create views to add, adjust and remove from cart

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,95 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.contrib import messages
 
 
 from store.models import Product
 
+
 def view_cart(request):
     """ A view to return the cart page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    product = get_object_or_404(Product, pk=item_id)
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+        messages.success(request,
+                         f'Updated {product.title} quantity to {cart[item_id]}'
+                         )
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {product.title} to your cart')
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {product.title} quantity to {cart[item_id]["items_by_size"][size]}')
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request,
+                             f'Removed size {size.upper()} {product.title} from your cart')
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, f'Updated {product.title} quantity to {cart[item_id]}')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.title} from your cart'
+                             )
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    product = get_object_or_404(Product, pk=item_id)
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.cart} from your cart')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
