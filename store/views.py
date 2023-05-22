6c84493d5b2e7807e418b6f840d31d5194c06073
henrysevern <henrysevern2@gmail.com>
2023-05-22 11:52:46 +0000

    Make styling changes

--- store/views.py
+++ store/views.py
@@ -25,9 +25,9 @@ def item_list(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'title':
+                sortkey = 'lower_title'
+                products = products.annotate(lower_title=Lower('title'))
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
