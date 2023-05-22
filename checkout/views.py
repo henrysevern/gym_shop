0a22a02dc16a5a790b57f39b5702f69ae8a92957
henrysevern <henrysevern2@gmail.com>
2023-05-22 08:58:44 +0000

    Fix bug on checkout

--- checkout/views.py
+++ checkout/views.py
@@ -167,7 +167,8 @@ def checkout_success(request, order_number):
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        email will be sent to {order.email}. \
+        You can also view your order in your profile')
 
     if 'cart' in request.session:
         del request.session['cart']
