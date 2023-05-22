0b1b03d8f9d69bcd64c7746e675661a137053b93
henrysevern <henrysevern2@gmail.com>
2023-05-22 13:05:24 +0000

    Change settings

--- gym_shop/settings.py
+++ gym_shop/settings.py
@@ -135,17 +135,22 @@ WSGI_APPLICATION = 'gym_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
+    'default': dj_database_url.parse('postgres://ghacuwws:zzjWXJafOc49qn2IZF7fZ7Bx9t0aQsA9@rogue.db.elephantsql.com/ghacuwws')
+}
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
@@ -201,7 +206,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+STRIPE_WH_SECRET = os.environ.get('')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 if 'USE_AWS' in os.environ:
