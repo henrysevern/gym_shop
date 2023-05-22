1efa315d24cc3e2abdba37a91eb3c22f9baa6216
henrysevern <henrysevern2@gmail.com>
2023-05-22 13:54:42 +0000

    Run new migrations

--- gym_shop/settings.py
+++ gym_shop/settings.py
@@ -63,9 +63,9 @@ INSTALLED_APPS = [
     'storages',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
