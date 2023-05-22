df7e8a302f9beafa2bc1a752e93b06c80496c626
henrysevern <henrysevern2@gmail.com>
2023-05-22 13:45:21 +0000

    Change settings for deployment

--- gym_shop/settings.py
+++ gym_shop/settings.py
@@ -135,22 +135,18 @@ WSGI_APPLICATION = 'gym_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-
-DATABASES = {
-    'default': dj_database_url.parse('postgres://ghacuwws:zzjWXJafOc49qn2IZF7fZ7Bx9t0aQsA9@rogue.db.elephantsql.com/ghacuwws')
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
